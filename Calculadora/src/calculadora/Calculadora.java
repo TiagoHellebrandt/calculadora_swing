008070651c602df1c4092e40e273f13ad58b923c
Tiago Hellebrandt Silva <thellebrandtsilva@gmail.com>
2018-06-10 15:04:51 -0400

    Calc

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -14,11 +14,13 @@ public final class Calculadora extends JFrame {
     private final TextField txt = new TextField(); // Campo de texto
     private final String[] names = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "+", "c", "0", "=", "-"}; // Nomes dos botões
     private final ArrayList<JButton> btns = new ArrayList(); // Botões
+    private double n = 0;
+    private String op = "";
     
     public Calculadora () {
-        this.setLayout(new GridLayout(5, 1)); // Define o layout
+        this.setLayout(new GridLayout(5, 1, 2, 2)); // Define o layout
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define a operação de fechar
-        this.setSize(400, 600); // Define o tamanho da janela
+        this.setSize(400, 500); // Define o tamanho da janela
         this.setVisible(true); // Define que a janela aparecerá
         // ADDs
         this.add(txt); // Adiciona o campo de texto na janela
@@ -28,13 +30,33 @@ public final class Calculadora extends JFrame {
     public void configTeclado() {
         for (String name : names) {
             JButton btn = new JButton(name); // Cria o botão
-            btn.addActionListener(new ActionListener(){
+            btn.addActionListener(new ActionListener(){ // Adiciona o evento no botão
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if ("=".equals(btn.getText()) || "c".equals(btn.getText())) {
+                    if ("=".equals(btn.getText()) || "c".equals(btn.getText()) || "*".equals(btn.getText()) || "/".equals(btn.getText()) || "+".equals(btn.getText()) || "-".equals(btn.getText())) {
                         if ("=".equals(btn.getText())) {
-                            txt.setText("Imagine aqui a resposta!");
+                            switch (op) {
+                                case "+":
+                                    txt.setText(String.valueOf(n+Double.parseDouble(txt.getText()))); // Soma dos números
+                                    break;
+                                
+                                case "-":
+                                    txt.setText(String.valueOf(n-Double.parseDouble(txt.getText()))); // Subtração dos números
+                                    break;
+                                
+                                case "*":
+                                    txt.setText(String.valueOf(n*Double.parseDouble(txt.getText()))); // Multiplicação dos números
+                                    break;
+                                    
+                                case "/":
+                                    txt.setText(String.valueOf(n/Double.parseDouble(txt.getText()))); // Divisão dos números
+                                    break;
+                            }
+                        } else if("c".equals(btn.getText())) {
+                            txt.setText("");
                         } else {
+                            n = Double.parseDouble(txt.getText());
+                            op = btn.getText();
                             txt.setText("");
                         }
                     } else {
@@ -46,26 +68,12 @@ public final class Calculadora extends JFrame {
         }
         for (int i=0,x=0;i<4;i++) {
             JPanel pn = new JPanel(); // Cria um painel para cada linha
-            pn.setLayout(new GridLayout(1, 4)); // Define o layout do painel da linha
+            pn.setLayout(new GridLayout(1, 4, 2, 2)); // Define o layout do painel da linha
             for (int j=0;j<4;j++) {
                 pn.add(btns.get(x)); // adiciona os botões aos seus paineis de linha
                 x++;
             }
             this.add(pn); // Adiciona cada painel de linha na janela
         }
-    }
-    
-    public static boolean isNumber(String x) {
-        if (!"+".equals(x) || !"-".equals(x) || !"*".equals(x) || !"/".equals(x)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public double calc(String input) {
-        double res = 0;
-        // CALCULA
-        return res;
     }
 }
